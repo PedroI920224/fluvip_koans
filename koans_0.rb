b79c51fd394a06674eecb71bf17253e4aa553184
Pedro ivan <pedro92_0224@hotmail.com>
2016-08-21 23:23:33 -0500

    modify the cicle

--- koans_0.rb
+++ koans_0.rb
@@ -1,6 +1,7 @@
 class Begin 
   def initializer()
   end
+
   def welcome
     text_0 = "welcome to Fluvip".center(80," ")
     puts text_0
@@ -8,47 +9,56 @@ class Begin
       print line
     end
   end
+
   def clear_scroll
     puts "                                                                                 "*399
   end
+
   def menu
     File.foreach('menu.txt') do |line|
       print line
       gets.strip
     end
   end
+
   def install_curl
     File.foreach('install_curl.txt') do |line|
       print line
       gets.strip
     end
   end
+  
   def install_rvm
     File.foreach('install_rvm.txt') do |line|
       print line
       gets.strip
     end
   end
+
   def install_tmate
     File.foreach('install_tmate.txt') do |line|
       print line
       gets.strip
     end
   end
+
   def install_postgres
     File.foreach('install_postgresql.txt') do |line|
       print line
       gets.strip
     end
   end
+
   def install_Mongo
   end
+
   def install_git
     File.foreach('install_git.txt') do |line|
       print line
       gets.strip
     end
   end
+
   def options
     options = gets.chomp()
     options = options
@@ -67,14 +77,22 @@ class Begin
       install_Mongo
      when "7"
       install_git
+     else
+      return 10
     end
   end
+
+  def loop_menu
+    loop do
+      clear_scroll
+      menu
+      a = options
+      break if a == 10
+    end
+  end
+
 end
 welcome = Begin.new()
 welcome.welcome
 gets()
-welcome.clear_scroll
-gets()
-welcome.menu
-welcome.options
-
+welcome.loop_menu
